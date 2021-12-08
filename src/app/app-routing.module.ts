c709303c5c4b47d0c8458b84a5388b468ad8a86b
Samuel Ramon <samuelrbo@gmail.com>
2021-12-08 22:34:56 +0000

    :rocket: Added app routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'settings',
+    loadChildren: () => import('./modules/settings/settings.module').then(m => m.SettingsModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules,
+    relativeLinkResolution: 'legacy'
+})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}

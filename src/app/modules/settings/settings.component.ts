d153c174fa5b3ea2b841401e104f1499f7ff201b
Samuel Ramon <samuelrbo@gmail.com>
2021-12-08 22:32:54 +0000

    :rocket: Change error type to check later

--- src/app/modules/settings/settings.component.ts
+++ src/app/modules/settings/settings.component.ts
@@ -11,7 +11,7 @@ import { User, UserService } from '../../core';
 export class SettingsComponent implements OnInit {
   user: User = {} as User;
   settingsForm: FormGroup;
-  errors: Object = {};
+  errors: any = {};
   isSubmitting = false;
 
   constructor(

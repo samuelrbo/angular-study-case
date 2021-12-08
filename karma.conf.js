fedd6da7edbc446395756cd3626348cfd538af0c
Samuel Ramon <samuelrbo@gmail.com>
2021-12-08 17:00:48 +0000

    :tada: Change project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/agendamento-consulta-form'),
+      dir: require('path').join(__dirname, './coverage/angular-study-case'),
       subdir: '.',
       reporters: [
         { type: 'html' },

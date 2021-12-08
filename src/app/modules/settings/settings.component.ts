445a8f41fe4ab954b40989b45695402040eae875
Samuel Ramon <samuelrbo@gmail.com>
2021-12-08 22:05:08 +0000

    :rocket: Settings module

---
+++ src/app/modules/settings/settings.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { User, UserService } from '../../core';
+
+@Component({
+  selector: 'app-settings-page',
+  templateUrl: './settings.component.html'
+})
+export class SettingsComponent implements OnInit {
+  user: User = {} as User;
+  settingsForm: FormGroup;
+  errors: Object = {};
+  isSubmitting = false;
+
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private fb: FormBuilder
+  ) {
+    this.settingsForm = this.fb.group({
+      image: '',
+      username: '',
+      bio: '',
+      email: '',
+      password: ''
+    });
+  }
+
+  ngOnInit() {
+    Object.assign(this.user, this.userService.getCurrentUser());
+    this.settingsForm.patchValue(this.user);
+  }
+
+  logout() {
+    this.userService.rejectAuth();
+    this.router.navigateByUrl('/');
+  }
+
+  submitForm() {
+    const that = this;
+
+    this.isSubmitting = true;
+    this.updateUser(this.settingsForm.value);
+
+    this.userService
+      .update(this.user)
+      .subscribe({
+        next(data: User) {
+          that.router.navigateByUrl('/profile/' + data.username);
+        },
+        error(err: any) {
+          that.errors = err;
+          that.isSubmitting = false;
+        }
+      }
+    );
+  }
+
+  updateUser(values: Object) {
+    Object.assign(this.user, values);
+  }
+}

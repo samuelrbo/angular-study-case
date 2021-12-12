58ca5edf6dca444eb3d063ab36742656f06d3ac9
Samuel Ramon <samuelrbo@gmail.com>
2021-12-12 15:07:01 +0000

    :sparkles: Article list

---
+++ src/app/shared/components/articles/article-list/article-list.component.ts
@@ -0,0 +1,64 @@
+import { Component, Input } from '@angular/core';
+
+import { Article, ArticleListFilter, ArticleService } from '../../../../core';
+
+@Component({
+  selector: 'app-article-list',
+  templateUrl: './article-list.component.html',
+  styleUrls: ['./article-list.component.css']
+})
+export class ArticleListComponent {
+  @Input() limit!: number;
+
+  @Input()
+  set filter(filter: ArticleListFilter) {
+    if (filter) {
+      this.query = filter;
+      this.currentPage = 1;
+      this.runQuery();
+    }
+  }
+
+  query!: ArticleListFilter;
+  results!: Article[];
+  loading = false;
+  currentPage = 1;
+  totalPages: Array<number> = [1];
+
+  constructor(
+    private articleService: ArticleService,
+  ) {}
+
+  setPage(pageNumber: number): void {
+    this.currentPage = pageNumber;
+    this.runQuery();
+  }
+
+  runQuery(): void {
+    const that = this;
+
+    this.loading = true;
+    this.results = [];
+
+    if (this.limit) {
+      this.query.filters.limit = this.limit;
+      this.query.filters.offset = this.limit * (this.currentPage - 1);
+    }
+
+    this.articleService.search(this.query)
+      .subscribe({
+        next(data) {
+          that.loading = false;
+          that.results = data.articles;
+
+          // Used from http://www.jstips.co/en/create-range-0...n-easily-using-one-line/
+          that.totalPages = Array.from(
+            new Array(
+              Math.ceil(data.articlesCount / that.limit)
+            ),
+            (_, index) => index + 1
+          );
+        }
+      });
+  }
+}
